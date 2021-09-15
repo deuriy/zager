ddf1037c8a1a278073c67c9afb0b90b1dfe8dfed
deuriy <deuriy@yandex.ru>
2021-09-15 21:52:23 +0300

    Video section layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,27 @@
-import { Example } from "./modules/example.js";
+// import {
+//   Example
+// } from "./modules/example.js";
 
 // new Example();
+
+import Swiper, {
+  Navigation,
+  Pagination
+} from '../../node_modules/swiper/swiper-bundle';
+
+new Swiper('.VideoSwiper', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 'auto',
+  spaceBetween: 48,
+
+  pagination: {
+    el: '.VideoSection_pagination',
+    clickable: true,
+    bulletClass: 'SwiperPagination_bullet',
+    bulletActiveClass: 'SwiperPagination_bullet-active',
+  },
+
+  navigation: {
+    nextEl: '.VideoSwiper_next',
+  },
+});
