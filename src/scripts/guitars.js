740c0541b0046a2d9b5f785adb205e11a380f664
deuriy <deuriy@yandex.ru>
2021-09-21 21:48:41 +0300

    Guitars banner layout

---
+++ src/scripts/guitars.js
@@ -0,0 +1,16 @@
+import Swiper, {
+  Navigation,
+  Pagination
+} from '../../node_modules/swiper/swiper-bundle';
+
+new Swiper('.IconsAndTextsSwiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  autoHeight: true,
+
+  // breakpoints: {
+  //   768: {
+  //     spaceBetween: 48
+  //   },
+  // },
+});
