056097f2cb88500be5730ebd66952ae2490cbb6d
deuriy <deuriy@yandex.ru>
2021-10-10 18:18:38 +0300

    Price section layout

--- src/scripts/lessons.js
+++ src/scripts/lessons.js
@@ -6,15 +6,33 @@ function slideToggle(elem) {
   }
 }
 
+function checkInfoLineHeight(infoLine) {
+  if (infoLine.classList.contains('InfoLine-halfOverlapTop')) {
+    infoLine.style.marginBottom = `-${infoLine.offsetHeight / 2}px`;
+  } else if (infoLine.classList.contains('InfoLine-halfOverlapBottom')) {
+    infoLine.style.marginTop = `-${infoLine.offsetHeight / 2}px`;
+  }
+}
+
+let infoLinesHalfOverlap = document.querySelectorAll('.InfoLine-halfOverlapTop, .InfoLine-halfOverlapBottom');
+infoLinesHalfOverlap.forEach(infoLine => {
+  checkInfoLineHeight(infoLine);
+});
+
+window.addEventListener('resize', function () {
+  infoLinesHalfOverlap.forEach(infoLine => {
+    checkInfoLineHeight(infoLine);
+  });
+});
+
 function setPostsVisibility(visible = true) {
   let prop = !visible ? 'none' : '';
   let posts = document.querySelectorAll('.PostsSection_item:nth-child(n+4)');
   posts.forEach(post => post.style.display = prop);
 }
 
-
 let postsSectionItems = document.querySelector('.PostsSection_items');
-if (postsSectionItems) {
+if (postsSectionItems && document.documentElement.clientWidth <= 767) {
   setPostsVisibility(false);
   postsSectionItems.style.maxHeight = postsSectionItems.offsetHeight + 'px';
   setPostsVisibility();
